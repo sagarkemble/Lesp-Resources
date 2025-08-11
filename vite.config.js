906cf8775e630b0ea28ecd4e412a3c3039a818ef
Sagar Kemble <sagarkemble.sasa@gmail.com>
2025-08-11 17:57:47 +0530

    inital commit .

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,12 @@ export default defineConfig({
         background_color: "#000000",
         icons: [
           {
-            src: "logo.png",
+            src: "manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "logo.png",
+            src: "manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
           },
